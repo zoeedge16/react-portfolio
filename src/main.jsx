fec8067b790cb0e7500b06769ba1827703ba1c70
zoe <zoeedge0703@gmail.com>
2023-09-11 15:51:53 -0500

    created the imports in the maina nd started the set up for the routing

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import ReactDOM from 'react-dom/client'
+// Bringing in the required imports from 'react-router-dom' to set up application routing behavior
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+const router = createBrowserRouter({
+    path: '/',
+    element: <App />,
+    errorElement: 
+})

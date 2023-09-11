7d4dcd6b9e3e5182ccdf24ebd6d6f86c1f1f0f3b
zoe <zoeedge0703@gmail.com>
2023-09-11 16:07:40 -0500

    implemented the rendering to the root seciton of the index.html

--- src/main.jsx
+++ src/main.jsx
@@ -10,3 +10,7 @@ const router = createBrowserRouter({
     path: '/',
     element: <App />
 })
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <RouterProvider router={router} />
+  );

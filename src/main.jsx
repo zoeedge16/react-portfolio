e2e68cdbe0d337d8723a4ba3e572570dbd4bbeed
zoe <zoeedge0703@gmail.com>
2023-09-11 16:23:30 -0500

    created the pages section and the about.jsx and added the needed formatting for the main

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,33 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import App from './App'
+import About from './pages/About'
 
-const router = createBrowserRouter({
-    path: '/',
-    element: <App />
-})
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />,
+        errorElement: <Error />,
+        children: [
+            {
+                index: true,
+                element: <About />
+            },
+            {
+                index: '/Portfolio',
+                element: <Portfolio />
+            },
+            {
+                index: '/Contact',
+                element: <Contact />
+            },
+            {
+                index: '/Resume',
+                element: <Resume />
+            }
+        ],
+    },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <RouterProvider router={router} />
---
+++ src/pages/About.jsx
@@ -0,0 +1,15 @@
+export default function About() {
+    return (
+        <div>
+            <h1> About Me </h1>
+            <p> 
+                I am an alumni of the University of Tennessee Knoxville. 
+                I have a major in Chemistry and a minor in Italian. 
+                I am currently attending Vanderbilt's Software Enginnering Bootcamp.
+                I work at Pace National as a Scientist.
+                My undergraduate research was with the computational chemistry department headed by Dr. Kostas Vogiatzis. 
+                The goal of the reasearch was to create a program that allowed scientists to visualize pharmacrophore moelcules in 3D space.
+                </p>
+        </div>
+    );
+}

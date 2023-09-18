a4a2aa87eb8992e4f2a2a4b0878de0aa1083bcbf
zoe <zoeedge0703@gmail.com>
2023-09-18 14:16:05 -0500

    changed the projects

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -4,28 +4,29 @@ export default function Portfolio() {
         <section class="border">
             <h2 id="work">Work</h2>
             <div class="work-items">
-                <a href="https://zoeedge16.github.io/code-refactor-seo/" class="work-item featured">
-                    <figcaption class="caption">Horiseon Debug</figcaption>
+                {/* fix the src here */}
+                <a href="https://zoeedge16.github.io/weather-webpage/" class="work-item featured">
+                    <figcaption class="caption">Weather Webpage</figcaption>
                     <figure class="photo">
-                        <img class="fluid-img" src="./assets/image/horiseon-seo.png" alt="Horiseon Debug" />
+                        <img class="fluid-img" src="" alt="Weaather Webpage" />
                     </figure>
                 </a>
-                <a href="https://zoeedge16.github.io/prework-study-guide/" class="work-item">
-                        <figcaption class="caption">Prework Study Guide</figcaption>
+                <a href="https://zoeedge16.github.io/password-generator/" class="work-item">
+                        <figcaption class="caption">Password Generator</figcaption>
                         <figure class="photo">
-                            <img class="fluid-img" src="./assets/image/prework-study-Guide.png" alt="Prework Study Guide" />
+                            <img class="fluid-img" src="" alt="Password Generator" />
                         </figure>    
                 </a>
-                <a href="https://zoeedge16.github.io/css-snippet-cheatsheet/" class="work-item">
-                        <figcaption class="caption">CSS Snippet Cheatsheet</figcaption>
+                <a href="https://safe-shelf-80683-3dfb2eba4d8a.herokuapp.com/notes" class="work-item">
+                        <figcaption class="caption">Note Taker</figcaption>
                         <figure class="photo">
-                            <img class="fluid-img" src="./assets/image/css-snippet-cheatsheet.png" alt="CSS Snippet Cheatsheet" />
+                            <img class="fluid-img" src="" alt="Note Taker" />
                         </figure>
                 </a>
-                <a href="https://rickster1999.github.io/drink-roulette-project/" class="work-item">
-                    <figcaption class="caption">Drink Roulette</figcaption>
+                <a href="https://frozen-tor-00446-d883d20dd32e.herokuapp.com/" class="work-item">
+                    <figcaption class="caption">Granular Garden</figcaption>
                     <figure class="photo">
-                        <img class="fluid-img" src="./assets/image/Drink-Roulette.png" alt="CSS Snippet Cheatsheet" />
+                        <img class="fluid-img" src="" alt="Granular Garden" />
                     </figure>
             </a>
             </div>
